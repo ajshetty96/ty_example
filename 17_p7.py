1a93ad77f94650504610d0a7e0ea10b5b59298b9
ajshetty96 <anushreeshetty604@gmail.com>
2025-07-05 15:39:48 +0530

    new

--- 17_p7.py
+++ 17_p7.py
@@ -1,6 +1,6 @@
 with open("log.txt")as f:
     content= f.read()
     if ("python"in content):
-        print("python is absent")
+        print("python is absent 1 2")
     else:
         print("python not present") 
